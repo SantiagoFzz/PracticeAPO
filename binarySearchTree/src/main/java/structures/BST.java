50ba2b175ae6dbc9f38d0e72e57cda889b2bf49a
SantiagoFzz <florezsolartes@outlook.com>
2025-04-09 10:31:17 -0500

    new methods

--- binarySearchTree/src/main/java/structures/BST.java
+++ binarySearchTree/src/main/java/structures/BST.java
@@ -53,6 +53,121 @@ public class BST<T extends Comparable<T>> {
         }
     }
 
+    // Delete
+    public boolean delete(T data){
+        boolean isDeleted = false;
+        if(root != null){
+            // Si el nodo a eliminar es la raiz.
+            if(root.getValue().compareTo(data) == 0){
+                // Si la raiz es el unicio nodo del arbol.
+                if(root.getRight() == null && root.getLeft() == null){
+                    root = null;
+                }
+                // La raiz tiene un unico hijo
+                if(root.getRight() != null && root.getLeft() == null){
+                    root = root.getRight();
+                }
+                else if(root.getRight() == null && root.getLeft() != null){
+                    root = root.getLeft();
+                }
+                else if(root.getLeft() == null && root.getRight() != null){
+                    Node<T> predecessor = getMax(root.getLeft());
+                    root.setValue(predecessor.getValue());
+                    delete(root, root.getLeft(), predecessor.getValue());
+                }
+            }
+            delete(null, root, data);
+        }
+        return isDeleted;
+    }
+
+    private boolean delete(Node<T> parent, Node<T> current, T goal){
+        boolean isDeleted = false;
+
+        // CASO BASE 1: Encontramos el nodo.
+        if(current.getValue().compareTo(goal) == 0){
+            // CASO BASE 1.1: es un nodo hoja.
+            if(current.getRight() == null && current.getLeft() == null){
+                // Evaluamos si es un hijo derecho y lo eliminamos.
+                if(parent.getRight() == current){
+                    parent.setRight(null);
+                }
+                // Es un hijo izquierdo, entonces lo eliminamos.
+                else{
+                    parent.setLeft(null);
+                }
+            }
+            // CASO BASE 1.1¿2.
+            // CASO BASE 1.2.1. Tiene unico hijo derecho
+            if(current.getRight() != null && current.getLeft() == null){
+                // Evaluamos si es un hijo derecho y lo eliminamos.
+                if(parent.getRight() == current){
+                    parent.setRight(current.getRight());
+                    current.setRight(null);
+                }
+                // Es un hijo izquierdo, entonces lo eliminamos.
+                else{
+                    parent.setLeft(current.getRight());
+                    current.setRight(null);
+                }
+            }
+            // CASO BASE 1.2.2. Tiene unico hijo izquierdo
+            else if(current.getRight() == null && current.getLeft() != null){
+                // Evaluamos si es un hijo izquierdo y lo eliminamos.
+                if(parent.getRight() == current){
+                    parent.setRight(current.getLeft());
+                    current.setLeft(null);
+                }
+                // Es un hijo derecho, entonces lo eliminamos.
+                else{
+                    parent.setLeft(current.getLeft());
+                    current.setLeft(null);
+                }
+            }
+            else if(current.getLeft() != null && current.getRight() != null){
+                Node<T> predecessor = getMax(current.getLeft());
+                // Evaluamos si es un hijo derecho y lo eliminamos.
+                if(parent.getRight() == current){
+                    parent.getRight().setValue(predecessor.getValue());
+                }
+                // Es un hijo izquierdo, entonces lo eliminamos.
+                else{
+                    parent.getLeft().setValue(predecessor.getValue());
+                }
+                delete(current, current.getLeft(), predecessor.getValue());
+            }
+        }
+        // CASO RECURSIVO
+        else if(current.getValue().compareTo(goal) > 0){
+            delete(current, current.getLeft(), goal);
+        }
+        else if(current.getValue().compareTo(goal) < 0){
+            delete(current, current.getRight(), goal);
+        }
+
+        return isDeleted;
+    }
+
+    // Get the max node.
+    public Node<T> getMax(){
+        Node<T> current = null;
+        if(root != null){
+            current = root;
+            while(current.getRight() != null){
+                current = current.getRight();
+            }
+        }
+        return current;
+    }
+
+    // Get the max node.
+    private Node<T> getMax(Node<T> current){
+        if(current.getRight() != null){
+            getMax(current.getRight());
+        }
+        return current;
+    }
+
     public Node<T> getRoot() {
         return root;
     }
--- binarySearchTree/src/main/java/ui/Main.java
+++ binarySearchTree/src/main/java/ui/Main.java
@@ -7,18 +7,18 @@ public class Main {
         BST<Integer> tree= new BST<>();
 
         tree.add(50);
-        tree.add(40);
-        tree.add(60);
         tree.add(30);
-        tree.add(20);
-        tree.add(55);
+        tree.add(40);
+        tree.add(45);
+        tree.add(70);
+        tree.add(60);
+        tree.add(75);
+        tree.add(65);
+        tree.add(73);
+        tree.add(80);
 
+        tree.delete(70);
         System.out.println(tree.getRoot().getValue());
-        System.out.println(tree.getRoot().getLeft().getValue());
-        System.out.println(tree.getRoot().getLeft().getLeft().getValue());
-        System.out.println(tree.getRoot().getLeft().getLeft().getLeft().getValue());
-
-        System.out.println(tree.getRoot().getRight().getLeft().getValue());
-
+        System.out.println(tree.getRoot().getRight().getValue());
     }
 }
